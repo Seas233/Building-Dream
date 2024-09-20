a2acbe54f3bb477556e49d8621679524578d9ab4
Seas233 <2438814689@qq.com>
2024-09-20 21:51:49 +0800

    大体完成了视频推荐页nanoda！、

--- views/archive_recommendation.vue
+++ views/archive_recommendation.vue
@@ -1,7 +1,40 @@
 <script setup>
+    import { useRouter } from 'vue-router';
     import axios from 'axios';
 
-    //档案推荐页面，将档案室按照最后更新时间顺序排列，添加进id="mi"中
+    const router = useRouter();
+
+    //获取list，一串档案室的id
+    //axios()
+
+    //根据档案室id查找
+    let archiverooms = [
+        {
+            team_name:'千年游戏开发部第一支教队',
+            name:"玩档玩的",
+            id:1,
+            create_time:"2024年8月21日",
+            latest_time:"2024年8月25日",
+            archive:[
+                {title:"日服结算室外GOZ大决战",id:112},
+                {title:"总力室内黑白TM一图流参考",id:113},
+                {title:"10.10-10.16 防御演习 室内重甲",id:113},
+                {title:"10.10-10.16 防御演习 室内重甲",id:113},
+            ]
+        },
+        {
+            team_name:'千年游戏开发部第一支教队',
+            name:"玩粥玩的",
+            id:2,
+            create_time:"2024年8月21日",
+            latest_time:"2024年8月25日",
+            archive:[
+                {title:"AS-S1~4低配平民全关卡攻略！",id:112},
+                {title:"AS-S1~5摆完半挂机全关卡攻略！",id:113},
+                {title:"小丘郡剿灭摆完挂机全关卡攻略！",id:113},
+            ]
+        }
+    ]
 </script>
 
 <template>
@@ -14,7 +47,23 @@
         <div></div>
 
         <div id="mi">
-            <div></div>
+            <div id="mi_head">档案推荐</div>
+            
+            <div class="room_" v-for="j in archiverooms">
+                <div class="team_head">{{ j["team_name"] }}</div>
+
+                <div class="room_head">档案室：{{ j["name"] }}</div>
+
+                <div class="room_time">创建：{{ j["create_time"] }}<p></p>更改：{{ j["latest_time"] }}</div>
+                
+                <!--档案-->
+                <div class="room_body">
+                    <div class="arc_" v-for="k in j['archive']" @click="router.push({path: '/archive',query: {arc_id: k['id']}})">
+                        <div class="arc_pic"></div>
+                        <div class="arc_title">{{ k["title"] }}</div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div></div>
@@ -42,7 +91,83 @@
 
 #mi{
     background-color: rgba(255,255,255,0.43);
-    padding: 100px;
+    padding-top: 30px;
     overflow: auto;
+}
+
+#mi>*{
+    width: 100%;
+
+    padding-left: 100px;
+    padding-right: 100px;
+}
+
+.team_head{
+    color: #6B944F;
+    font-size: 2em;
+    font-weight: 900;
+
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+
+    margin-bottom: 30px;
+}
+
+#mi_head{
+    color: #6B944F;
+    font-size: 2em;
+    font-weight: 900;
+
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+
+    margin-bottom: 30px;
+}
+
+.room{
+    margin-bottom: 30px;
+}
+
+.room_head{
+    font-size: 1.5em;
+    color: #6B944F;
+    margin-bottom: 15px;
+}
+
+.room_time{
+    margin-bottom: 15px;
+}
+
+.room_body{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-gap: 25px;
+    padding-bottom: 15px;
+}
+
+.arc_{
+    width: 200px;
+    height: 200px;
+    background-color: #b3f0a2;
+    padding: 20px;
+    border-radius: 20px;
+}
+
+.arc_pic{
+    background-image: url("/档案.png");
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: auto 100px;
+    height: 100px;
+    width: 100%;
+    margin-bottom: 10px;
+}
+
+.arc_title{
+    font-size: 1.1em;
 }
 </style>
